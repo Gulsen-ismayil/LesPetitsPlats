c1358518bfbe89bbbc4f8ad5f80de9cfd4b49849
Gvl <gulixiansimayi@gmail.com>
2022-09-09 16:28:50 +0200

    cardrecipes

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 // import { getRecipes } from "./utils/util.js";
 // import { init } from "./utils/util.js";
+async function init() {
+    const {recipes} = await getRecipes();
+    displayData(recipes)
+}
+await init();
 
- async function getRecipes() {
+async function getRecipes() {
     const data = await fetch('data/recipes.json');
     const dataRecipes = await data.json()
-
+    
     return dataRecipes;
-}
-
-
-function recipesFactory(data) {
-    const {name,time,ingredients,description} = data
-
-    function getUserCardDom() {
-        const nameRecipes = document.querySelector('.name');
-        nameRecipes.innerHTML = name;
-
-        const timeRecipes = document.querySelector('.time');
-        const clock = document.querySelector('.fa-clock');
-        const minute = document.querySelector('.minute');
-        minute.innerText = time+'min';
-
-        const text = document.querySelector('.text');
-        const detail = document.querySelector('.detail');
-        const descriptionRecipes = document.querySelector('.description');
-        let liste;
-        ingredients.forEach(element => {
-        liste =  `${element.ingredient} : ${element.quantity} ${element.unit}`
-        });
-        detail.innerText = liste
-        console.log(ingredients);
-        descriptionRecipes.innerText = description;
-
-    }
-    return {name,getUserCardDom}
 }
 
 
 function displayData(recipes) {
     const sectionRecipes = document.getElementById('section-recipe');
     recipes.forEach(recipe => { 
-        const factoryRecipes = recipesFactory(recipe)
-        const recipesDOM = factoryRecipes.getUserCardDom()
         console.log(recipe);
+        let factoryRecipes = recipesFactory(recipe)
+        let recipesDOM = factoryRecipes.getUserCardDom();
         sectionRecipes.appendChild(recipesDOM)
     });
 }
 
-async function init() {
-   const {recipes} = await getRecipes();
-   displayData(recipes)
+
+function recipesFactory(data) {
+    const {name,time,ingredients,description} = data
+    function getUserCardDom() {
+        const card = document.querySelector('.card')
+        const nameRecipes = document.querySelector('.name');
+        nameRecipes.innerHTML = name;
+        
+        const timeRecipes = document.querySelector('.time');
+        const clock = document.querySelector('.fa-clock');
+        const minute = document.querySelector('.minute');
+        minute.innerText = time+'min';
+        
+        const text = document.querySelector('.text');
+        const detail = document.querySelector('.detail');
+        const descriptionRecipes = document.querySelector('.description');
+        let liste;
+        ingredients.forEach(element => {
+            liste +=  `${element.ingredient} : ${element.quantity} ${element.unit}`
+        });
+        detail.innerText = liste
+        console.log(ingredients);
+        descriptionRecipes.innerText = description;
+        
+        return (card)
+        
+    }
+    return {name,getUserCardDom}
 }
-await init();
+
+
