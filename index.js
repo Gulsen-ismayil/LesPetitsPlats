6d6cf3bc5f646267a3647e9eedd77ab8fff82972
Gvl <gulixiansimayi@gmail.com>
2022-09-27 11:38:23 +0200

    input search is ok, start to three button

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-// import { getRecipes } from "./utils/util.js";
-// import { init } from "./utils/util.js";
-async function init() {
-    const {recipes} = await getRecipes();
-    displayData(recipes)
-}
-await init();
+const sectionRecipes = document.getElementById('section-recipe');
+const {recipes} = await getRecipes();
+updateUI(recipes);
 
 async function getRecipes() {
     const data = await fetch('data/recipes.json');
     const dataRecipes = await data.json()
-    
     return dataRecipes;
 }
 
-function recipesFactory(recipe) {
-    const sectionRecipes = document.getElementById('section-recipe');
-    const {name,time,ingredients,description} = recipe
-    function getUserCardDom() {
-        let liste;
-        let i
-        for(i=0;i<ingredients.length;i++){
-            liste = `${ingredients[i].ingredient}:${ingredients[i].quantity}${ingredients[i].unit}`
-           console.log(i);
-        }
-        
+function recipesFactory(data) {
+    const {name,time,ingredients,description} = data
+    let liste = ''
+    for(let i=0;i<ingredients.length;i++){
+        liste = liste + `${ingredients[i].ingredient}:${ingredients[i].quantity}${ingredients[i].unit || '' } <br>`
+    }
+
+    function recipesCardDom() {
         let code =`
         <div class="card">
         <div class="image" ></div>
@@ -43,39 +35,165 @@ function recipesFactory(recipe) {
         </div>
         `
         sectionRecipes.innerHTML +=code;
-        // const card = document.querySelector('.card')
-        // const nameRecipes = document.querySelector('.name');
-        // nameRecipes.innerHTML = name;
-        
-        // const timeRecipes = document.querySelector('.time');
-        // const clock = document.querySelector('.fa-clock');
-        // const minute = document.querySelector('.minute');
-        // minute.innerText = time+'min';
-        
-        // const text = document.querySelector('.text');
-        // const detail = document.querySelector('.detail');
-        // const descriptionRecipes = document.querySelector('.description');
-        // let liste;
-        // ingredients.forEach(element => {
-            //     liste +=  `${element.ingredient} : ${element.quantity} ${element.unit}`
-            // });
-            // detail.innerText = liste
-            // console.log(ingredients);
-            // descriptionRecipes.innerText = description;
-            
-            // return (card)
-            
-        }
-        return {name,getUserCardDom}
     }
-    function displayData(recipes) {
-        recipes.forEach(recipe => { 
-            const factoryRecipes = recipesFactory(recipe)
-            factoryRecipes.getUserCardDom();
-            // sectionRecipes.appendChild(recipesDOM)
-            // sectionRecipes.innerHTML +=code;
-        });
+    return {recipesCardDom}
+}
+
+
+// input recherche 
+
+let inputSearch = document.querySelector('input');
+inputSearch.addEventListener('change',() =>{updateUI(recipes)})
+
+function displayRecipes(recipes) {
+    sectionRecipes.innerHTML = ''
+
+    recipes.forEach(recipe => { 
+    const factoryRecipes = recipesFactory(recipe)
+    factoryRecipes.recipesCardDom();
+    });
+}
+
+// sectionRecipes.innerHTML = ''
+// let filteredRecipes =[];
+// let inputSearch = document.querySelector('input');
+
+
+function filterSearch(){
+    let filteredRecipes =[];
+    const text = document.querySelector('.input-search').value;
+    filteredRecipes = recipes.filter((recipe) => {
+        if(recipe.name.includes(text)||
+           recipe.description.includes(text)||
+           recipe.ingredients.some(({ingredient}) => {  // un objet . si non on peut ecrire aussi : ingredient => { return ingredient.ingredient.includes()}
+               return ingredient.includes(text)})){
+                   return true
+               }
+    })  
+    return filteredRecipes
+}
+
+function updateUI(recipes) {
+    let filteredRecipes = filterSearch(recipes)
+    displayRecipes(filteredRecipes)
+  }
+
+//   section par type 
+const filterAll = document.querySelector('.filter')
+const filterIngredients = document.querySelector('.filter1');
+const filterApplications = document.querySelector('.filter2');
+const filterUstensiles = document.querySelector('.filter3');
+
+filterIngredients.addEventListener('click',filterType);
+filterApplications.addEventListener('click',filterType);
+filterUstensiles.addEventListener('click',filterType);
+
+function filterType(e) {
+    if(e.target.className === 'filter filter1'){
+        displayFilterType(nameOfIngredient,filterIngredients)
+        filterIngredients.classList.replace('filter','filterAll')
+        // filterApplications.style.display = 'none';
+        // filterUstensiles.style.display = 'none';
+        console.log('ingredient')
+    }else if(e.target.className === 'filter filter2'){
+        displayFilterType(nameOfAppliance,filterApplications)
+        filterApplications.classList.replace('filter','filterAll')
+        // filterIngredients.style.display = 'none';
+        // filterUstensiles.style.display = 'none';
+        console.log('appreil');
+    }else if(e.target.className === 'filter filter3'){
+        displayFilterType(nameOfUstensils,filterUstensiles)
+        filterUstensiles.classList.replace('filter','filterAll')
+        // filterApplications.style.display = 'none';
+        // filterIngredients.style.display ='none';
+        console.log('ustensiles');
     }
-    
-    
-    
+}
+
+// ingredient 
+
+const nameOfIngredient = nameIngredient();
+const nameOfAppliance = nameAppliance();
+const nameOfUstensils = nameUstensils();
+
+function displayFilterType(nameOfFilterType,blocOfFilterType) {
+    const code = ` 
+    <div class="bloc-ingredient">
+      <input type="search" class="input-ingredient" placeholder="Rechercher un ingrédient">
+      <i class="fa-solid fa-angle-up"></i>
+    </div>
+    <ul class="card-nameIngredient"></ul>
+    `
+    blocOfFilterType.innerHTML = code;
+    nameOfFilterType.forEach(name=>{
+        const eachIngredientUl = document.querySelector('ul');
+        const eachIngredientLi = document.createElement('li');
+        eachIngredientLi.classList.add('each-ingredientLi')
+        eachIngredientLi.innerText = name;
+
+        eachIngredientUl.appendChild(eachIngredientLi);
+    })
+
+}
+
+function nameIngredient(){
+    let arrayIngredientBeforeSet = [] 
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            arrayIngredientBeforeSet.push(ingredient.ingredient)
+        })
+    })
+    let objectNameIngredient = new Set(arrayIngredientBeforeSet);//creer nouveau tableau without doublont
+    const arrayIngredientAfterSet = Array.from(objectNameIngredient)
+   
+    return arrayIngredientAfterSet
+}
+
+// appliance
+
+function nameAppliance() {
+    let arrayApplianceBeforeSet = []
+    recipes.forEach(recipe=> {
+        arrayApplianceBeforeSet.push(recipe.appliance)
+    })
+    let objectNameAppliance = new Set(arrayApplianceBeforeSet);
+    const arrayApplianceAfterSet = Array.from(objectNameAppliance)
+    return arrayApplianceAfterSet
+}
+
+// ustensiles 
+
+function nameUstensils() {
+    let arrayUstensilsBeforeSet = []
+    recipes.forEach(recipe=>{
+        recipe.ustensils.forEach(ustensil=>{
+            arrayUstensilsBeforeSet.push(ustensil)
+        })
+    })
+    let objectNameUstensils = new Set(arrayUstensilsBeforeSet)
+    const arrayUstensilsAfterSet = Array.from(objectNameUstensils)
+    return arrayUstensilsAfterSet
+}
+
+
+// function displayIngredient() {
+//     const nameOfIngredient = nameIngredient()
+//     // filterIngredients.classList.add('filter-ingredient');
+//     const code = ` 
+//     <div class="bloc-ingredient">
+//       <input type="search" class="input-ingredient" placeholder="Rechercher un ingrédient">
+//       <i class="fa-solid fa-angle-up"></i>
+//     </div>
+//     <ul class="card-nameIngredient"></ul>
+//     `
+//     filterIngredients.innerHTML = code;
+//     nameOfIngredient.forEach(name=>{ 
+//         const eachIngredientUl = document.querySelector('ul');
+//         const eachIngredientLi = document.createElement('li');
+//         eachIngredientLi.classList.add('each-ingredientLi')
+//         eachIngredientLi.innerText = name;
+
+//         eachIngredientUl.appendChild(eachIngredientLi);
+//         filterIngredients.appendChild(eachIngredientUl);
+//     }) 
+// }
